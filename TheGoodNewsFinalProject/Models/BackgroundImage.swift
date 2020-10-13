fc0c865070a4ccde035ec1b9031c4e4e7f6c0118
RJ <r.j.chomik@gmail.com>
2020-10-13 18:20:08 +0100

    getBackgroundImage func moved into Backgrounds class from ViewController

--- TheGoodNewsFinalProject/Models/BackgroundImage.swift
+++ TheGoodNewsFinalProject/Models/BackgroundImage.swift
@@ -6,14 +6,33 @@
 //  Copyright © 2020 The App Experts. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Kingfisher
 
+class Backgrounds {
+    public func getBackgroundImage(imageView: UIImageView) {
+        var backgrounds:[URL] = []
+        guard let url = URL(string: "https://pixabay.com/api/?key=18691967-c6bbf9bfa8dba2ffd4c907bb5&q=beautiful+landscape&image_type=photo") else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else{return}
+            guard let results = try? JSONDecoder().decode(BackgroundImages.self, from: data) else {return}
+            for hit in results.hits {
+                backgrounds.append(hit.webformatURL)
+                print("JSON> \(hit.webformatURL)")
+            }
+            DispatchQueue.main.async {
+                let randomInt = Int.random(in: 1..<backgrounds.count)
+                imageView.kf.setImage(with: backgrounds[randomInt], placeholder: UIImage(imageLiteralResourceName:"landscape"))
+            }
+            
+        }.resume()
+    }
+}
 
 struct BackgroundImages: Decodable {
     let hits:[BackgroundImage]
 }
 
 struct BackgroundImage:Decodable {
-//    let id:Int
     let webformatURL:URL
 }
--- TheGoodNewsFinalProject/ViewController.swift
+++ TheGoodNewsFinalProject/ViewController.swift
@@ -20,13 +20,11 @@ class ViewController: UIViewController {
     
     var googleAdsManager = GoogleAdsManager()
     var banner:GADBannerView!
-    var backgrounds:[BackgroundImage] = []
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setBanner()
-        getBackgroundImage()
+        Backgrounds().getBackgroundImage(imageView: backgroundImageView)
     }
     
     override func viewDidLayoutSubviews() {
@@ -34,21 +32,8 @@ class ViewController: UIViewController {
         banner.frame = googleAdsManager.getBannerSize(size: view.frame.size).integral
     }
     
-    func getBackgroundImage() {
-        guard let url = URL(string: "https://pixabay.com/api/?key=18691967-c6bbf9bfa8dba2ffd4c907bb5&q=beautiful+landscape&image_type=photo") else {return}
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else{return}
-            guard let results = try? JSONDecoder().decode(BackgroundImages.self, from: data) else {return}
-            for hit in results.hits {
-                self.backgrounds.append(hit)
-                print("JSON> \(hit)")
-            }
-//            print("data> \(data)")
-//            print("resp> \(response)")
-//            print("erro> \(error)")
-            
-        }.resume()
-    }
+
+    
     
     func setBanner() {
         self.banner = googleAdsManager.getBanner()
