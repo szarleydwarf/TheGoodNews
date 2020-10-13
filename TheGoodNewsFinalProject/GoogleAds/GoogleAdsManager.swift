36d61a635be69ef163978db124ab9bf8b260e3f6
RJ <r.j.chomik@gmail.com>
2020-10-13 11:38:12 +0100

    Google Ads SDK added

---
+++ TheGoodNewsFinalProject/GoogleAds/GoogleAdsManager.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleAdsManager.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 13/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+import GoogleMobileAds
+import Foundation
---
+++ TheGoodNewsFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 13/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
