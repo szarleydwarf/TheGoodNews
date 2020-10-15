81fc882e38e9a068d21d7e88c50dec89ed3d9963
RJ <r.j.chomik@gmail.com>
2020-10-15 18:33:00 +0100

    added Poem view start implementation, have issue vith background imageview

---
+++ TheGoodNewsFinalProject/Views/PoemsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PoemsViewController.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 15/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class PoemsViewController: UIViewController {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var poemTitleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var poemLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveFavourite(_ sender: UIButton) {
+    }
+    
+    @IBAction func sharePoem(_ sender: Any) {
+    }
+}
