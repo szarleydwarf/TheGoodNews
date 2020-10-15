0c6902fa6adc022b6f86318824922922aa806d4a
RJ <r.j.chomik@gmail.com>
2020-10-15 14:18:32 +0100

    started implementation of settings view

---
+++ TheGoodNewsFinalProject/Views/SettingsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewController.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 15/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+    
+}
