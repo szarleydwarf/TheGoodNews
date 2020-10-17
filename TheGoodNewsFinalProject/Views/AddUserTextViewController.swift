7d57c927c76e00b36792cd78191e15db543dad26
RJ <r.j.chomik@gmail.com>
2020-10-17 15:57:44 +0100

    adding user text storyboard view added, started implementation

---
+++ TheGoodNewsFinalProject/Views/AddUserTextViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddUserTextViewController.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 17/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class AddUserTextViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
