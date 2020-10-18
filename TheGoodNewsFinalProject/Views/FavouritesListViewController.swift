51ee6cbf3439675151a75ca41fa10664bffcb308
RJ <r.j.chomik@gmail.com>
2020-10-18 15:40:33 +0100

    testing displaying favourites in one view with one table, using segmentControl to decide what to display in table

---
+++ TheGoodNewsFinalProject/Views/FavouritesListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FavouritesListViewController.swift
+//  TheGoodNewsFinalProject
+//
+//  Created by The App Experts on 18/10/2020.
+//  Copyright © 2020 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    @IBOutlet weak var table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
